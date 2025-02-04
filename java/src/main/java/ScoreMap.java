007fa83d22043636cb62f156995fa03bd2cfbb01
Tadiwanashe Dzvoti <tadiwanashe.dzvoti01@thg.com>
2025-02-04 16:01:17 +0000

    Finished scores map

--- java/src/main/java/ScoreMap.java
+++ java/src/main/java/ScoreMap.java
@@ -3,6 +3,9 @@ import java.util.Map;
 
 public class ScoreMap {
     private static final Map<Integer, String> scoreMap = Map.ofEntries(
-            new AbstractMap.SimpleEntry<Integer, String>(0, "Love")
-    )
+            new AbstractMap.SimpleEntry<Integer, String>(0, "Love"),
+            new AbstractMap.SimpleEntry<Integer, String>(1, "Fifteen"),
+            new AbstractMap.SimpleEntry<Integer, String>(2, "Thirty"),
+            new AbstractMap.SimpleEntry<Integer, String>(3, "Forty")
+    );
 }
